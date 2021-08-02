161bc949af6adffdb4094afd9c74e699d065b749
Bruna Lima <lbruna886@gmail.com>
2021-08-02 11:02:13 -0300

    1064 - Positivos e Média

---
+++ 1064.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+	
+	double valor, soma, media, positivos;
+	
+	for(int i = 0; i < 6; i++) {
+		cin >> valor;
+		if (valor > 0) {
+			positivos += 1; 
+			soma += valor;
+		}
+	}
+	
+	cout << positivos << " valores positivos" << endl;
+	
+	media = soma / positivos;
+		
+	cout << fixed << setprecision(1) << media << endl;
+		
+	return 0;
+}
+
