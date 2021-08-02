d22887df6764948fc610b4854589322cf770d660
Bruna Lima <lbruna886@gmail.com>
2021-08-01 22:13:51 -0300

    Questão 1001 - Extremamente Básico

---
+++ 1001.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	
+	int a, b, prod = 0;
+	
+	cin >> a >> b;
+	
+	prod = a*b;
+	
+	cout << "PROD = " << prod << endl;
+	
+	return 0;
+	
+}
