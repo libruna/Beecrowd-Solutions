79b8b7657c435f9f1569e3d3094b2879d3204b13
Bruna Lima <lbruna886@gmail.com>
2021-08-02 15:37:11 -0300

    Questão 1930 - Tomadas

---
+++ 1930.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	
+	int t1, t2, t3, t4, soma = 0;
+	
+	cin >> t1 >> t2 >> t3 >> t4;
+	
+	soma = (t1 + t2 + t3 + t4) - 3;
+	
+	cout << soma << endl;
+		
+	return 0;
+}
