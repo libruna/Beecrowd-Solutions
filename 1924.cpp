437da223455e1b30f895939b5f0f06ed42be26dc
Bruna Lima <lbruna886@gmail.com>
2021-08-02 15:38:12 -0300

    Questão 1924 - Vitória e a Indecisão

---
+++ 1924.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	
+	int n;
+	string nome;
+	
+	cin >> n;
+	
+	for(int i = 0; i < n; i++) {
+		cin >> nome;
+	}
+	cout << "Ciencia da Computacao" << endl;
+		
+	return 0;
+}
