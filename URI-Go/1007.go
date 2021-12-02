2c0255fbbaf89e18693fe5bca60965eff8a19624
bruna <lbruna886@gmail.com>
2021-12-02 11:44:18 -0300

    Questão 1007 - Diferença

---
+++ URI-Go/1007.go
@@ -0,0 +1,18 @@
+package main
+ 
+import (
+    "fmt"
+)
+ 
+func main() {
+
+   	var a, b, c, d, dif int
+
+	fmt.Scanf("%d", &a)
+	fmt.Scanf("%d", &b)
+	fmt.Scanf("%d", &c)
+	fmt.Scanf("%d", &d)
+
+	dif = (a * b) - (c * d)
+	fmt.Printf("DIFERENCA = %d\n", dif)
+}
