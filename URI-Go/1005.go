dcf66df44d189ad98684672daa59e46fdc5d2cc2
bruna <lbruna886@gmail.com>
2021-12-02 15:18:30 -0300

    Questão 1005 - Média 1

---
+++ URI-Go/1005.go
@@ -0,0 +1,15 @@
+package main
+ 
+import (
+    "fmt"
+)
+ 
+func main() {
+	var a, b, media float64
+
+	fmt.Scanf("%f", &a)
+	fmt.Scanf("%f", &b)
+
+	media = ((a * 3.5) + (b*7.5))/11
+	fmt.Printf("MEDIA = %.5f\n", media)
+}
