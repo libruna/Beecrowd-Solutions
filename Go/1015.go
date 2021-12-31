eea6b16442a4fac33a9052a4d63a026c319eb9fa
bruna <lbruna886@gmail.com>
2021-12-31 11:41:10 -0300

    iniciante

---
+++ Go/1015.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	var x1, x2, y1, y2 float64
+
+	fmt.Scanf("%f", &x1)
+	fmt.Scanf("%f", &y1)
+	fmt.Scanf("%f", &x2)
+	fmt.Scanf("%f", &y2)
+
+	distancia := math.Sqrt(math.Pow(x2-x1, 2) + math.Pow(y2-y1, 2))
+	fmt.Printf("%.4f\n", distancia)
+}
