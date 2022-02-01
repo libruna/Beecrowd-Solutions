bf18d0bce022ee10f0fca152a313614ecffaaff4
Luis Eduardo Ribeiro Guerra <luiseduardoribeiroguerra@gmail.com>
2022-01-31 21:17:44 -0300

    Create 1002.hs

---
+++ Haskell/1002.hs
@@ -0,0 +1,15 @@
+-- https://www.beecrowd.com.br/judge/pt/problems/view/1001
+-- Comment
+{- Multiline comments can be enclosed
+in a block like this.
+-}
+
+
+main = do
+    a <- getLine --Read answer from terminal
+
+    putStr "A=" -- Print text without break line
+
+    -- "read variableName :: Double" convert string to float with double precision
+
+    putStrLn $ show ( ( (read a :: Double) ^ 2 ) * 3.14159 )
