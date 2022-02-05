12fb69dafa70d03b848bab45c2eaee29ba018fe8
Luis Eduardo Ribeiro Guerra <luiseduardoribeiroguerra@gmail.com>
2022-02-05 15:17:16 -0300

    Create 1016.hs

---
+++ Haskell/1016.hs
@@ -0,0 +1,12 @@
+-- https://www.beecrowd.com.br/judge/pt/problems/view/1001
+{- Multiline comments can be enclosed
+in a block like this.
+-}
+
+main = do
+    a <- readLn :: IO Int --Read answer from terminal as Integer
+
+    let result = a * 2
+    
+    putStr $ show ( result  ) -- Print text without break line
+    putStrLn " minutos" -- Print text with break line
